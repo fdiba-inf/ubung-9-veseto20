c40872b52e50524b7f8f58c86cbb0c809b5306f3
veseto20 <vesela_krumova2001@abv.bg>
2020-11-26 07:43:55 +0000

    ellipse

--- src/exercise9/EllipseDemo.java
+++ src/exercise9/EllipseDemo.java
@@ -2,7 +2,12 @@ package exercise9;
 
 public class EllipseDemo {
     public static void main(String[] args) {
-      
+
+      Ellipse ellipse1 = new Ellipse();
+
+      Point point2 = new Point(x:2,y:3);
+      Ellipse ellipse2 = new Ellipse(point2,a:5,b:6);
+       
 
     }
 }
